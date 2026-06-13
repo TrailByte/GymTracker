446f478feceb5ad4109359202c152b6706e60c86
TrailByte <tra1lbyte@proton.me>
2026-06-13 22:50:44 +0300

    Dependencies added

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation(libs.androidx.navigation.compose)
 }
